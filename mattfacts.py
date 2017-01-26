4a8c8fc9fa7da6465cf7cb7c299e48892e16d6c4
Trevor Wakeman <trevor.wakeman@colorado.edu>
2017-01-25 21:18:31 -0700

    added strings for lol responses

--- mattfacts.py
+++ mattfacts.py
@@ -36,10 +36,12 @@ def handle_command(command, channel):
         random_fact_number = randint(0, len(ALT_RESPONSES) - 1)
         response = ALT_RESPONSES[random_fact_number]
     elif command.startswith(RANK):
-        for item in games['31203597']:
-            tier = item['tier']
-            division = item['entries'][0]['division']
-            response = tier, division
+        tier = games.json()['31203597'][1]['tier']
+        division = games.json()['31203597'][1]['entries'][0]['division']
+        #for item in games['31203597']:
+            #tier = item['tier']
+            #division = item['entries'][0]['division']
+        response = tier + division
     else:
         response = "Sure...write some more code then I can do that!"
     slack_client.api_call("chat.postMessage", channel=channel,
